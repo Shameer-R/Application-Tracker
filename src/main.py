f3a44e9907fbfb01881b581944c224b90cbd2d78
Shameer-R <roseshammy@gmail.com>
2025-06-01 18:17:58 -0700

    Setup basic function placeholders

--- src/main.py
+++ src/main.py
@@ -1,5 +1,21 @@
+import database.Database
+from database.Database import Database
+
+application_database = database.Database.Database()
+def starting_prompt():
+    starting_input = input("Select Key: \n1. Add an application\n2. Update an application\n")
+    if starting_input == "1":
+        add_application()
+    elif starting_input == "2":
+        update_application()
+
+def add_application():
+    pass
+def update_application():
+    pass
+
 def main():
-    print("Hello World")
+    starting_prompt()
 
 
 if __name__ == "__main__":
