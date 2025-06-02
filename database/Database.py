3f75146edf6de3b2049bbe726893017752814a28
Shameer-R <roseshammy@gmail.com>
2025-06-01 19:33:07 -0700

    Add data insertion

--- database/Database.py
+++ database/Database.py
@@ -1,7 +1,7 @@
 import sqlite3
 
 create_table_query = '''
-CREATE TABLE IF NOT EXISTS Application (
+CREATE TABLE IF NOT EXISTS Applications (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   company TEXT NOT NULL,
   position TEXT NOT NULL,
@@ -16,3 +16,16 @@ class Database:
             cursor.execute(create_table_query)
             connection.commit()
             print("Application Database intialized successfully")
+
+    def insert_application(self, company, position, status, date_applied, notes):
+        with sqlite3.connect('../database/application_database.db') as connection:
+            cursor = connection.cursor()
+            insert_query = '''
+            INSERT INTO Applications (company, position, status, date_applied, notes)
+            VALUES (?, ?, ?, ?, ?)
+            '''
+            cursor.execute(insert_query, (company, position, status, date_applied, notes))
+            connection.commit()
+            cursor.close()
+
+            print("Application inserted successfully")
--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import database.Database
 from database.Database import Database
 
@@ -9,8 +11,43 @@ def starting_prompt():
     elif starting_input == "2":
         update_application()
 
+
+ApplicationDictionary = {
+    "company": None,
+    "position": None,
+    "status": None,
+    "date_applied": None,
+    "notes": None
+}
+
+def prompt_to_database(prompt, field):
+    new_prompt = input(prompt).replace(" ", "") # Remove whitespace from prompt
+    ApplicationDictionary[field] = new_prompt
+
+def insert_data_into_database():
+    application_database.insert_application(
+        ApplicationDictionary["company"], ApplicationDictionary["position"],
+        ApplicationDictionary["status"], ApplicationDictionary["date_applied"],
+        ApplicationDictionary["notes"]
+    )
+
+
+def format_todays_date():
+    today = str(datetime.today())
+    today_format = today.split(" ")
+    return today_format[0]
+
 def add_application():
-    pass
+    print("\n--- Enter Application Information --- ")
+    prompt_to_database("Enter Company: ", "company")
+    prompt_to_database("Enter Position: ", "position")
+    prompt_to_database("Enter Notes (Leave blank if none): ", "notes")
+
+    ApplicationDictionary["status"] = "Pending"
+    ApplicationDictionary["date_applied"] = format_todays_date()
+
+    insert_data_into_database()
+
 def update_application():
     pass
 
