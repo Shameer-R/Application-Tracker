c39caa99b2f8e0e14aef7cceea68af762b406ef9
Shameer-R <roseshammy@gmail.com>
2025-06-01 19:50:02 -0700

    Add function to get all applications in database

--- database/Database.py
+++ database/Database.py
@@ -22,10 +22,24 @@ class Database:
             cursor = connection.cursor()
             insert_query = '''
             INSERT INTO Applications (company, position, status, date_applied, notes)
-            VALUES (?, ?, ?, ?, ?)
+            VALUES (?, ?, ?, ?, ?);
             '''
             cursor.execute(insert_query, (company, position, status, date_applied, notes))
             connection.commit()
             cursor.close()
 
             print("Application inserted successfully")
+
+    def get_all_applications(self):
+        with sqlite3.connect('../database/application_database.db') as connection:
+            cursor = connection.cursor()
+            select_query = '''
+            SELECT * FROM Applications;'''
+
+            cursor.execute(select_query)
+
+            all_applications = cursor.fetchall()
+
+            print(all_applications)
+
+            cursor.close()
