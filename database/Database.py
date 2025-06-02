63c82e6c6e59a3c5ab15a6cd17ac95e8776095c3
Shameer-R <roseshammy@gmail.com>
2025-06-01 18:17:37 -0700

    Setup SQL Initialization

--- database/Database.py
+++ database/Database.py
@@ -1,7 +1,18 @@
 import sqlite3
-class Database:
-    def Connect(self):
-        connection = sqlite3.connect('application_database.db')
-        cursor = connection.cursor()
 
-        return cursor
+create_table_query = '''
+CREATE TABLE IF NOT EXISTS Application (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  company TEXT NOT NULL,
+  position TEXT NOT NULL,
+  status TEXT NOT NULL,
+  date_applied DATE NOT NULL,
+  notes TEXT
+);'''
+class Database:
+    def __init__(self):
+        with sqlite3.connect('../database/application_database.db') as connection:
+            cursor = connection.cursor()
+            cursor.execute(create_table_query)
+            connection.commit()
+            print("Application Database intialized successfully")
